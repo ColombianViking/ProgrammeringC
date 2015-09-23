75d4da6b292c0925e1bb07fe7030ee49590b2c1f
Jesper Karjalainen <jeska043@li17-5.ida.liu.se>
2015-09-23 15:23:28 +0200

    second commit

---
+++ Monetary.h
@@ -0,0 +1,30 @@
+//Monetary.h
+#include <string>
+
+#ifndef MONEY_H
+#define MONEY_H
+
+namespace monetary
+{
+
+class Money
+{
+private:
+    std::string currency;
+    int units, hundreds; 
+
+public:
+    Money (std::string a, int b, int c) : currency{a}, units{b}, hundreds{c} {};
+      /* Money (int);
+    Money (int, int);
+    Money (std::string);
+    Money (std::string, int);
+    Money (std::string, int, int);
+    Money (Money&);*/
+    std::string cur () {return currency;}
+    int uni () {return units;}
+    int hun () {return hundreds;}
+};
+
+}
+#endif
