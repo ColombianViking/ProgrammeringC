d61a0be485ff1c79661169c10336146769da8f8c
SwedishCocaine <johnstynsberg@gmail.com>
2015-09-24 11:51:25 +0200

    makefile skapad och lab2 utvidgad med test av konstruktorer och utskrifter

--- lab2.cc
+++ lab2.cc
@@ -2,23 +2,24 @@
 #include "Monetary.h"
 #include <iostream>
 using namespace std;
+using namespace monetary;
 
 int main()
 {
-    monetary::Money m1{"SEK",13,37};
+  cout << endl << "m1{\"SEK\",13,37}";
+  Money m1{"SEK",13,37};
+  cout << endl << "m2{m1}";
+  Money m2{m1};
+  cout << endl << "m3{\"USD\"}";
+  Money m3{"USD"};
+  cout << endl << "m4{15}" << endl;
+  Money m4{15};
 
-    cout << endl << "m1 består av följande:" << endl
-         << "Valuta: " << m1.cur() << endl 
-         << "Enheter: " << m1.uni() << endl 
-         << "Hundradelar: " << m1.hun() << endl<< endl;
-
-    monetary::Money m2{m1};
-
-    cout << "m2 blir en kopia av m1." << endl
-         << endl << "m2 består nu av:" << endl
-         << "Valuta: " << m2.cur() << endl 
-         << "Enheter: " << m2.uni() << endl 
-         << "Hundradelar: " << m2.hun() << endl<< endl;
+  cout << "m1.print skriver ut" << endl;
+  m1.print(cout);
+  cout << endl << "Utskrift av m1 med << operatorn ger" << endl;
+  cout << m1 << endl;
+  
 
     return 0;
 }
